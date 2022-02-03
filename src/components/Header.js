f989aa776d32d6349dc29cb375806089e4061295
Victor Hugo <victorhsmsantos@gmail.com>
2022-02-03 18:10:15 -0300

    Requisito 3 feito!

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 import Loading from './Loading';
 import { getUser } from '../services/userAPI';
 
@@ -35,6 +36,11 @@ class Header extends Component {
           {'Olá, '}
           <span data-testid="header-user-name">{user.name}</span>
         </h2>
+        <div>
+          <Link to="/search" data-testid="link-to-search">Pesquisa</Link>
+          <Link to="/favorites" data-testid="link-to-favorites">Favoritas</Link>
+          <Link to="/profile" data-testid="link-to-profile">Perfil</Link>
+        </div>
       </>
     );
   }
